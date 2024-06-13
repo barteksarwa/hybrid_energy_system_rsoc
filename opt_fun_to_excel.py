dce8d4b682c069f15f29c31676e5981011103b6c
barteksarwa <125754186+barteksarwa@users.noreply.github.com>
2024-06-13 23:16:45 +0200

    Update opt_fun_to_excel.py

--- opt_fun_to_excel.py
+++ opt_fun_to_excel.py
@@ -7,17 +7,14 @@ from openpyxl.styles import Alignment
 # Input directory for CSV files
 csv_directory = 'doe_output_csv_lower'
 
-# Output Excel file path for sums
-output_sums_excel_path = 'sums_output_update_lower.xlsx'
+# Path to the 'designs' file
+designs_file_path = 'designs_fff_lower.xlsx'
 
 # Path to the 'denormalized_designs' file
 designs_denormalized_file_path = 'denormalized_designs_fff_lower.xlsx'
 
-# Path to the 'designs' file
-designs_file_path = 'designs_fff_lower.xlsx'
-
-# Optimization file 'designs_with_losses' path
-optimization_file_path = 'designs_with_losses_fff_lower.xlsx'
+# Output file 
+output_sums_path = 'output.xlsx'
 
 # List all CSV files in the input directory with the name pattern 'pv_plot'
 csv_files = ([f for f in os.listdir(csv_directory) if f.startswith('pv_plot') and f.endswith('.csv')])
@@ -43,8 +40,6 @@ else:
         df = df[:-1]
         # Read the CSV file into a DataFrame
         sum_positive_indexes = np.sum(np.where(df.iloc[:, -3] > 0, 1, 0))
-        # print(df.iloc[:, -3])
-        # print(sum_positive_indexes)
         sum_positive_indexes_list.append(sum_positive_indexes)
         df_np = df.to_numpy()
         s_row = np.sum(df_np, axis=0)
@@ -54,7 +49,7 @@ else:
 
     dfs = np.array(dfs)
     dfs = dfs[:, -3:]/1000
-    # print(dfs)
+    
     # Recalculate energy loss and deficit into hydrogen
     energy_deficit = dfs[:, 0:1]
     energy_content_hydrogen = 33.3 # kWh / kg
@@ -79,28 +74,23 @@ else:
     cost_capex = np.sum(denom_designs_df.iloc[:, :4].values * costsunit_i +
                         denom_designs_df.iloc[:, :4].values * (costs_r * costsunit_i) 
                         + costs_install, axis=1)[:, np.newaxis]
-    # print(cost_capex)
     total_costs = cost_capex + np.sum(costs_m * 30)
-    # print(total_costs)
     dfs = np.concatenate((dfs, total_costs), axis=1)
 
 
     # Objective functions
     LPSR = np.array([value / 8760 for value in sum_positive_indexes_list]).reshape(-1, 1)
-    # print(LPSR)
     F1 = total_costs + (hydrogen_amount_kg*30*price_hydrogen_kg)  \
         + (hydrogen_amount_kg_loss*30*price_hydrogen_kg*0.4)
     # F1 = costs_and_hydrogen # * (1-PRi/8760)
     F2 = total_costs # * PRi / 8760
     dfs = np.concatenate((dfs, F1, F2, LPSR), axis=1)
-    # print(F2)
-    # print(F1)
-    np.savetxt("doe_lower_2035.csv", dfs, delimiter=" ", header="PV Battery SOFC TANK energy_deficit Energy_loss System_cost F1 F2 LPSR")
+    np.savetxt(output_sums_path, dfs, delimiter=" ", header="PV Battery SOFC TANK energy_deficit Energy_loss System_cost F1 F2 LPSR")
 
     result_param = [24, 19, 21, 12]
     cost_capex_u = np.sum(result_param * costsunit_i +
                         result_param * (costs_r * costsunit_i)
                         + costs_install)
-    # print(cost_capex)
     total_costs_u = cost_capex_u + np.sum(costs_m * 30)
-    print(total_costs_u)
+
+    
